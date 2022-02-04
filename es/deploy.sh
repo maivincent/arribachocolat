f2b6c40f8bd333179fb9e1e1cc3633b97e37f898
MaiVincent <vincent.mai@umontreal.ca>
2022-02-03 23:24:42 -0500

    Espagnol et Arawi

---
+++ es/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ -z "$1" ]]; then
+  echo "Please enter a git commit message"
+  exit
+fi
+
+cd _site && \
+echo "www.arribachocolat.ca" >> CNAME && \
+git add . && \
+git commit -m "$1" && \
+git push origin gh-pages && \
+cd .. && \
+echo "Successfully built and pushed gh-pages to Github."
